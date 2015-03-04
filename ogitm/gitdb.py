c6f9df15fefc7a8396ad098dad9c00f6eef805d9
Jonathan <jonathan.frere@gmail.com>
2015-03-04 17:24:24 +0000

    PyGit2 wrapper layer

--- ogitm/gitdb.py
+++ ogitm/gitdb.py
@@ -2,10 +2,59 @@ import pygit2 as pg2
 import json
 
 
-class GitDB:
+SIGNATURE = pg2.Signature('OGITM', '-')
 
-    AUTHOR = pg2.Signature('-', '-')
-    COMMITTER = pg2.Signature('-', '-')
+
+class TreeWrapper:
+
+    def __init__(self, repo):
+        self._repo = repo
+        self._tree = self._make_new_tree()
+
+    def __setitem__(self, name, text):
+        blob_id = self._repo.create_blob(text)
+        self._tree.insert(name, blob_id, pg2.GIT_FILEMODE_BLOB)
+
+    def __getitem__(self, name):
+        entry = self._tree.get(name)
+        if entry is None:
+            raise KeyError('{name} not in current tree'.format(name=name))
+        return self._repo[entry.id].data.decode('utf-8')
+
+    def __delitem__(self, name):
+        self._tree.remove(name)
+
+    def get(self, name, default=None):
+        try:
+            return self[name]
+        except KeyError:
+            return default
+
+    def clear(self):
+        self._tree.clear()
+
+    def save(self, msg=''):
+        tid = self._tree.write()
+        self._repo.create_commit(
+            'refs/heads/master', SIGNATURE, SIGNATURE,
+            '', tid, self._get_parents())
+        self._tree = self._make_new_tree()
+
+    def _get_parents(self):
+        if self._repo.is_empty:
+            return []
+        else:
+            return [self._repo.head.target]
+
+    def _make_new_tree(self):
+        if self._repo.is_empty:
+            return self._repo.TreeBuilder()
+        else:
+            old_tree = self._repo[self._repo.head.target].tree
+            return self._repo.TreeBuilder(old_tree)
+
+
+class GitDB:
 
     def _get_next_id(self):
         if hasattr(self, "_id"):
@@ -17,24 +66,16 @@ class GitDB:
 
     def __init__(self, location):
         self.repo = pg2.init_repository(location, bare=True)
-        if self.repo.is_empty:
-            self.current_tree = self.repo.TreeBuilder()
-        else:
-            self.current_tree = self.repo.TreeBuilder()
-        self.current_tree = self.repo.TreeBuilder()
+        self.current_tree = TreeWrapper(self.repo)
 
     def insert(self, document):
         d_id = self._get_next_id()
-        txt = json.dumps(document, indent=2)
-        d_oid = self.repo.create_blob(txt)
-        self.current_tree.insert(str(d_id), d_oid, pg2.GIT_FILEMODE_BLOB)
-
-        self.save('insert ' + str(d_id))
+        self.current_tree['doc-{id}'.format(id=d_id)]
+        self.current_tree.save('insert doc-{id}'.format(id=d_id))
         return d_id
 
     def save(self, msg='-'):
-        tree_id = self.current_tree.write()
-        commit_id = self.repo.create_commit(
-          'master', self.AUTHOR, self.COMMITTER,
-          msg, tree_id, [])
-        self.current_tree = self.repo.TreeBuilder()
+        return self.current_tree.save()
+
+    def get(self, doc_id):
+        return None
--- tests/test_gitdb.py
+++ tests/test_gitdb.py
@@ -1,4 +1,78 @@
 from ogitm import gitdb
+import pygit2
+import pytest
+
+
+class TestTreeWrapper:
+
+    @pytest.fixture
+    def gittree(self, tmpdir):
+        git = pygit2.init_repository(str(tmpdir), bare=True)
+        return gitdb.TreeWrapper(git)
+
+    def test_instantiation(self, tmpdir):
+        git = pygit2.init_repository(str(tmpdir), bare=True)
+        tree = gitdb.TreeWrapper(git)
+        assert tree
+
+    def test_inserting_and_removing(self, gittree):
+        gittree['name'] = 'text'
+        assert gittree['name'] == 'text'
+        assert gittree.get('name') == 'text'
+        del gittree['name']
+        with pytest.raises(KeyError):
+            gittree['name']
+        assert gittree.get('name') is None
+
+    def test_clear(self, gittree):
+        gittree['bob'] = 'Joe Bloggs'
+        assert gittree['bob'] == 'Joe Bloggs'
+        gittree.clear()
+        assert gittree.get('bob') is None
+        with pytest.raises(KeyError):
+            gittree['bob']
+
+    def test_saving_data(self, gittree):
+        gittree['doo-dah'] = 'yabadabadoo'
+        gittree.save()
+
+        assert gittree['doo-dah'] == 'yabadabadoo'
+        assert gittree.get('doo-dah') == 'yabadabadoo'
+        del gittree['doo-dah']
+        with pytest.raises(KeyError):
+            gittree['doo-dah']
+        assert gittree.get('doo-dah') is None
+
+    def test_saving_over_multiple_instances(self, tmpdir):
+        tree = self.gittree(tmpdir)
+        tree['gaggle'] = 'baggle'
+        tree.save()
+
+        tree2 = self.gittree(tmpdir)
+        assert tree2['gaggle'] == 'baggle'
+        assert tree2.get('gaggle') == 'baggle'
+        del tree2['gaggle']
+        with pytest.raises(KeyError):
+            tree2['gaggle']
+        assert tree2.get('gaggle') is None
+
+    def test_deletion_save(self, tmpdir):
+        tree = self.gittree(tmpdir)
+        tree['boggle'] = 'goggle'
+        tree.save()
+        assert tree['boggle'] == 'goggle'
+
+        del tree['boggle']
+        with pytest.raises(KeyError):
+            tree['boggle']
+
+        tree.save()
+        with pytest.raises(KeyError):
+            tree['boggle']
+
+        tree2 = self.gittree(tmpdir)
+        with pytest.raises(KeyError):
+            tree2['boggle']
 
 
 def test_gitdb(tmpdir):
