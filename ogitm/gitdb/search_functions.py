a060588e89bc2f5ea98d1fa0b2cb3249aabce62c
Jonathan <jonathan.frere@gmail.com>
2015-03-15 17:29:06 +0000

    Document search function

--- ogitm/gitdb/search_functions.py
+++ ogitm/gitdb/search_functions.py
@@ -5,11 +5,58 @@ __all__ = ['SearchFunction']
 
 
 class SearchFunction:
+    """A list of all search functions.
+
+    To add a function, use the :py:meth:`.SearchFunction.add` decorator.  This
+    passes the function itself through (so multiple invocations of the add
+    decorator can be called on the same item), and appends it to the internal
+    store of search functions.
+
+    To get a function, use the :py:meth:`.SearchFunction.get` classmethod.
+    This returns the function described by the given name, or raises KeyError
+    if no function is available.
+
+    This class should basically be used as a singleton instance - all methods
+    are class methods, and operate on a shared store of data.  This probably
+    isn't best practice, but it works for now.
+
+    See :py:meth:`.SearchFunction.add` for details on what a search function
+    should actually look like.
+    """
 
     funcs = {}
 
     @classmethod
     def add(cls, *funcnames):
+        """Add a function to the current list of functions.
+
+        The function should have the following signature:
+
+        :param any key: The key for which a value should be found.  This is
+            usually not important - the function is also passed the index that
+            pertains to this particular key.
+
+        :param str operator: The operator/name that this function has been
+            called under.  Sometimes it is simpler if different operators all
+            map to the same function (for example, all string methods map to
+            one function that dynamically calls the method on a string
+            instance).  This can be then used to work out the specific
+            operation.
+
+        :param any argument: The argument passed to this particular operator.
+
+        :param index: The index related to the key being searched against.
+            This is basically a dict mapping every value that has been assigned
+            to this key to a list of the ids of the documents where this
+            key-value mapping exists.
+        :type index: dict[any: list[id]]
+
+        :param set[id] all: The set of all ids that are currently stored.
+            This is useful in the case where you want to search for, say,
+            non-existance of a key, in which case the set of ids that should
+            be returned is the set of all ids that aren't in the index that
+            the function has been passed.
+        """
         def _add(func):
             for name in funcnames:
                 cls.funcs[name] = func
